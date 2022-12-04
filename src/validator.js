79187addd95b12d32bf9e87ce48c267ed6660af3
Johana Gonzales <j.gonzalesrav@gmail.com>
2022-12-04 15:33:45 -0500

    segundo commit

--- src/validator.js
+++ src/validator.js
@@ -2,4 +2,14 @@ const validator = {
   // ...
 };
 
+const botonInicial = document.getElementById("pay");
+let creditCardNumber = document.getElementById("creditCard");
+
+botonInicial.addEventListener("click", convert);
+
+function convert (val){
+    val = creditCardNumber.value.split('').reverse().map(Number);
+    console.log(val);
+}
+
 export default validator;
