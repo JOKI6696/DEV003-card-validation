6967130ed8f93008c70d5a73490074746b11c015
Johana Gonzales <j.gonzalesrav@gmail.com>
2022-12-06 12:25:29 -0500

    tercer commit, ya corre un poco, falta corregir que no lee addOdd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const botonInicial = document.getElementById("pay");
+
+let ccNumber = document.getElementById("creditCard");
+
+botonInicial.addEventListener("click", function (){
+if (validator.isValid(ccNumber.value)) {
+        alert("Gracias por su compra")
+      } else {
+        alert("Upss, por favor revise los datos ingresados")
+      }
+}
+);
+
--- src/validator.js
+++ src/validator.js
@@ -1,15 +1,34 @@
 const validator = {
-  // ...
-};
+  //Traer input del # de tarjeta
+  isValid : function cardValidation(ccNumber){
+    //Voltear los dígitos y convertir a array
+    let ccReversed = ccNumber.split("").reverse().map(Number);
+    //Crear variables para la suma
+    let addEven = 0;
+    let addOdd = 0;
+    //Función for para realizar los cálculos
+    for (let i=1; i<=ccReversed.length; i++){
+    //Para números en posición par
+    if (i % 0 === 0){
+      //Si la multiplicación es de dos cifras
+        addOdd += ccReversed[i];
+    //Para números en posicón impar
+    } else{
+      if(ccReversed[i] * 2 > 9){
+        addEven += ccReversed[i] * 2 - 9;
+      //Si la multiplicación es de una cifra
+      } else{
+        addEven += ccReversed[i] * 2;
+      }
+    }
+    }
+  //Retorna si es válido o no (T/F)
+  return (addEven + addOdd) % 10 === 0
+  }
 
-const botonInicial = document.getElementById("pay");
-let creditCardNumber = document.getElementById("creditCard");
-
-botonInicial.addEventListener("click", convert);
-
-function convert (val){
-    val = creditCardNumber.value.split('').reverse().map(Number);
-    console.log(val);
+  , maskify : function hideNumber (ccNumber) {
+    return ccNumber.slice(0, -4).replace(/[0-9]/g, '*').concat(ccNumber.slice(-4, ccNumber.len));
+  }
 }
 
 export default validator;
